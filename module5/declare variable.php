d8613c1c1af20711ad31196f70829caff1893f97
milanrathod24 <159526748+milanrathod24@users.noreply.github.com>
2025-05-11 22:16:49 +0530

    Add files via upload

---
+++ module5/declare variable.php
@@ -0,0 +1,19 @@
+<?php
+// Integer variable
+$age = 30;
+
+// Float (or double) variable
+$price = 19.99;
+
+// String variable
+$name = "Milan Rathod";
+
+// Boolean variable
+$is_member = true;
+
+// Display the values using echo
+echo "Name: " . $name . "<br>";
+echo "Age: " . $age . "<br>";
+echo "Price: $" . $price . "<br>";
+echo "Membership Status: " . ($is_member ? "Active" : "Inactive") . "<br>";
+?>
